dd59841b550ffa253b33690aec4938f523e4d15f
Kenneth <alcanamay@gmail.com>
2023-06-04 18:16:15 -0600

    AudioBG
    musica de fondo waos

---
+++ Proyectil/Assets/Scripts/Audio1.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio1 : MonoBehaviour
+{
+    private AudioSource _audioSource;
+    private GameObject[] other;
+    private bool NotFirst = false;
+    private void Awake()
+    {
+        other = GameObject.FindGameObjectsWithTag("Music");
+ 
+        foreach (GameObject oneOther in other)
+        {
+            if (oneOther.scene.buildIndex == -1)
+            {
+                NotFirst = true;
+            }
+        }
+ 
+        if (NotFirst == true)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(transform.gameObject);
+        _audioSource = GetComponent<AudioSource>();
+    }
+}
